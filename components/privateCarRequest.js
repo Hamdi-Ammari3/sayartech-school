72dcc753dbb16decf3b570e97d06f79ca95c3c59
Hamdi Ammari <elammari2022@gmail.com>
2024-11-27 07:07:30 +0100

    updating car request page

--- components/privateCarRequest.js
+++ components/privateCarRequest.js
@@ -6,9 +6,9 @@ import { addDoc, collection,Timestamp } from 'firebase/firestore';
 import { DB } from '../firebaseConfig'
 
 const cars = [
-  {label: 'سيارة خاصة صالون ',value: 'سيارة خاصة صالون ' },
-  {label:'سيارة خاصة ٧ راكب ',value:'سيارة خاصة ٧ راكب '},
-  {label:'ستاركس ',value:'ستاركس '},
+  {label: 'سيارة صالون ٥ راكب',value: 'سيارة صالون ٥ راكب'},
+  {label:'سيارة خاصة ٧ راكب',value:'سيارة خاصة ٧ راكب'},
+  {label:'ستاركس',value:'ستاركس'},
   {label:'باص صغير ١٢ راكب',value:'باص صغير ١٢ راكب'},
   {label:'باص متوسط ١٤ راكب',value:'باص متوسط ١٤ راكب'},
   {label:'باص كبير ٣٠ راكب',value:'باص كبير ٣٠ راكب'}
@@ -16,10 +16,12 @@ const cars = [
 
 const PrivateCarRequest = () => {
 
-  const [date, setDate] = useState(null);
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
   const [startTime, setStartTime] = useState(null);
   const [endTime, setEndTime] = useState(null);
   const [numberOfCars, setNumberOfCars] = useState(0);
+  const [numberOfPersons, setNumberOfPersons] = useState(0);
   const [carType, setCarType] = useState(null);
   const [schoolName,setSchoolName] = useState('')
   const [loading, setLoading] = useState(false);
@@ -33,7 +35,7 @@ const PrivateCarRequest = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (!date || !startTime || !endTime || !numberOfCars || !carType) {
+    if (!startDate || !endDate || !startTime || !endTime || !numberOfCars || !numberOfPersons || !carType) {
       setError('الرجاء التاكد من ادخال جميع البيانات');
       return;
     }
@@ -42,17 +44,17 @@ const PrivateCarRequest = () => {
   
     // Combine the selected date with the times for accurate datetime storage
     const combinedStartTime = new Date(
-      date.getFullYear(),
-      date.getMonth(),
-      date.getDate(),
+      startDate.getFullYear(),
+      startDate.getMonth(),
+      startDate.getDate(),
       startTime.getHours(),
       startTime.getMinutes()
     );
     
     const combinedEndTime = new Date(
-      date.getFullYear(),
-      date.getMonth(),
-      date.getDate(),
+      endDate.getFullYear(),
+      endDate.getMonth(),
+      endDate.getDate(),
       endTime.getHours(),
       endTime.getMinutes()
     );
@@ -60,18 +62,22 @@ const PrivateCarRequest = () => {
     try {
       await addDoc(collection(DB, 'carRequest'), {
         sender:schoolName,
-        date: Timestamp.fromDate(new Date(date.setHours(0, 0, 0, 0))), // Date part only
-        startTime: Timestamp.fromDate(combinedStartTime), // Exact start time
-        endTime: Timestamp.fromDate(combinedEndTime), // Exact end time
+        startDate: Timestamp.fromDate(new Date(startDate.setHours(0, 0, 0, 0))),
+        endDate: Timestamp.fromDate(new Date(endDate.setHours(0, 0, 0, 0))),
+        startTime: Timestamp.fromDate(combinedStartTime),
+        endTime: Timestamp.fromDate(combinedEndTime),
         numberOfCars,
+        numberOfPersons,
         carType: carType.value,
-        request_send_date: Timestamp.fromDate(new Date()), // Current timestamp
+        request_send_date: Timestamp.fromDate(new Date()),
       });
       alert('تم ارسال الطلب بنجاح');
-      setDate(null);
+      setStartDate(null);
+      setEndDate(null)
       setStartTime(null);
       setEndTime(null);
       setNumberOfCars(0);
+      setNumberOfPersons(0);
       setCarType(null);
     } catch (err) {
       alert('حدث خطا ما الرجاء المحاولة مرة ثانية');
@@ -89,44 +95,71 @@ const PrivateCarRequest = () => {
       <div className='private_car_request_form_div'>
       <form onSubmit={handleSubmit} className='private_car_request_form'>
         <div className='private_car_request_form_date'>
-        <div className='private_car_request_form_date_day'>
-          <DatePicker 
-          selected={date} 
-          onChange={(date) => setDate(date)} 
-          className='private_car_request_form_date_day_input'
-          placeholderText= 'اليوم'
-          />
-        </div>
-        <div className='private_car_request_form_date_time'>
-          <DatePicker
-            selected={startTime}
-            onChange={(time) => setStartTime(time)}
-            showTimeSelect
-            showTimeSelectOnly
-            timeIntervals={15}
-            timeCaption="Start Time"
-            dateFormat="h:mm aa"
+
+          <div className='private_car_request_form_date_day'>
+          <div>
+            <DatePicker 
+            selected={startDate} 
+            onChange={(date) => setStartDate(date)} 
             className='private_car_request_form_date_day_input'
-            placeholderText='وقت الحضور'
-          />
-        </div>
-        <div>
-          <DatePicker
-            selected={endTime}
-            onChange={(time) => setEndTime(time)}
-            showTimeSelect
-            showTimeSelectOnly
-            timeIntervals={15}
-            timeCaption="End Time"
-            dateFormat="h:mm aa"
+            placeholderText= 'من يوم'
+            />
+          </div>
+          <div>
+            <DatePicker 
+            selected={endDate} 
+            onChange={(date) => setEndDate(date)} 
             className='private_car_request_form_date_day_input'
-            placeholderText='وقت المغادرة'
-          />
-        </div>
+            placeholderText= 'الى يوم'
+            />
+          </div>
+          </div>
+
+          <div className='private_car_request_form_date_day'>
+          <div>
+            <DatePicker
+              selected={startTime}
+              onChange={(time) => setStartTime(time)}
+              showTimeSelect
+              showTimeSelectOnly
+              timeIntervals={15}
+              timeCaption="Start Time"
+              dateFormat="h:mm aa"
+              className='private_car_request_form_date_day_input'
+              placeholderText='وقت الحضور'
+            />
+          </div>
+          <div>
+            <DatePicker
+              selected={endTime}
+              onChange={(time) => setEndTime(time)}
+              showTimeSelect
+              showTimeSelectOnly
+              timeIntervals={15}
+              timeCaption="End Time"
+              dateFormat="h:mm aa"
+              className='private_car_request_form_date_day_input'
+              placeholderText='وقت المغادرة'
+            />
+          </div>
+          </div>
+          
         </div>
         
         <div className='private_car_request_form_car'>
           
+          <div className='private_car_request_form_car_carNumber'>
+            <h5>عدد الحضور</h5>
+            <input
+              type="number"
+              value={numberOfPersons}
+              onChange={(e) => setNumberOfPersons(e.target.value)}
+              min="1"
+              required
+              className='private_car_request_form_date_day_input'
+            />
+          </div>
+
           <div className='private_car_request_form_car_carNumber'>
             <h5>عدد السيارات</h5>
             <input
